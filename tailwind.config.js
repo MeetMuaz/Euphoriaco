c8ed69084eca95ef52ffb36b24a3cf8c1443c554
Muaz Balogun <muazbalogun97@gmail.com>
2026-02-16 11:20:31 +0100

    final commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./public/index.html",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          DEFAULT: '#ec4899', // Pink
-          dark: '#db2777',
-          light: '#f9a8d4',
-        },
-        secondary: {
-          DEFAULT: '#fbbf24', // Yellow
-          dark: '#f59e42',
-          light: '#fef3c7',
-        },
-        accent: {
-          DEFAULT: '#a78bfa', // Purple
-          dark: '#7c3aed',
-          light: '#ddd6fe',
-        },
-      },
-      fontFamily: {
-        display: ["Poppins", "sans-serif"],
-        body: ["Inter", "sans-serif"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
